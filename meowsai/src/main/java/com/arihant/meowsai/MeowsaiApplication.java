9b6541568d27253ab89863b0e81dc96f40debb26
Arihant Kamde <59204121+arihantXD@users.noreply.github.com>
2025-08-07 16:20:04 +0530

    project setup

---
+++ meowsai/src/main/java/com/arihant/meowsai/MeowsaiApplication.java
@@ -0,0 +1,13 @@
+package com.arihant.meowsai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowsaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeowsaiApplication.class, args);
+	}
+
+}
